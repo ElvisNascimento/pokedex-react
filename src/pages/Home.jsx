aff4f8b84efa1f0386687f8c764b12602cfd5b09
ElvisNascimento <elvisnascimentodev@gmail.com>
2022-10-26 23:46:34 -0300

    funcinal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,69 +5,60 @@ import Navbar from "../components/Navbar";
 import PokemonCard from "../components/PokemonCard";
 
 export const Home = () => {
-    const [startGen,setStartGen] = useState([]);
-    const [endGen,setEndGen] = useState([]);
+    const [startGen, setStartGen] = useState();
+    const [endGen, setEndGen] = useState();
     const [pokemons, setPokemons] = useState([]);
     const [generation, setGeneration] = useState(1);
-
-    useEffect ( async () => {
-        getPokemons()
-        console.log("chamando a funcao pokemons:" + startGen, endGen);
-
-    }, [startGen,endGen]);
 
     const onChangeGenerations = (generation) => {
         setGeneration(generation);
     }
-    const getGeneration = (generation) => {
-
-        if (generation === 1) {
+    const getPokemons = async () => {
+     if (generation === 1) {
             //total de 1 a 151
-            setStartGen( 1);
-            setEndGen (151);
+            setStartGen(1);
+            setEndGen(151);
         }
         if (generation === 2) {
             //total de 152 a 251
-            setStartGen( 152);
-            setEndGen (251);
+            setStartGen(152);
+            setEndGen(251);
         }
         if (generation === 3) {
             //total de 252 a 386
-            setStartGen( 252);
-            setEndGen (386);
+            setStartGen(252);
+            setEndGen(386);
         }
         if (generation === 4) {
             //total de 387 a 493
-            setStartGen( 387);
-            setEndGen (493);
+            setStartGen(387);
+            setEndGen(493);
         }
         if (generation === 5) {
             //total de 494 a 649
-            setStartGen( 494);
-            setEndGen (649);
+            setStartGen(494);
+            setEndGen(649);
         }
         if (generation === 6) {
             //total de 650 a 721
-            setStartGen( 650);
-            setEndGen (659);
+            setStartGen(650);
+            setEndGen(721);
         }
         if (generation === 7) {
             //total de 722 a 809
-            setStartGen( 722);
-            setEndGen (731);
+            setStartGen(722);
+            setEndGen(809);
         }
         if (generation === 8) {
             //total de 810 a 900
-            setStartGen( 810);
-            setEndGen (819);
+            setStartGen(810);
+            setEndGen(900);
         }
-
-    }
-    const getPokemons = async () => {
-        getGeneration(generation);
+        setGeneration(generation);
         let endPoints = [];
-        for (let i = startGen; i <= endGen ; i++) {
-            endPoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+        for (let i = startGen; i <= endGen; i++) {
+            console.log(startGen,endGen);
+            endPoints.push(`https://pokeapi.co/api/v2/pokemon/${i}`)
         }
         await axios.all(endPoints.map((endpoint) => axios.get(endpoint)))
             .then((res) => setPokemons(res)).catch((error) => console.log(error));
@@ -85,6 +76,9 @@ export const Home = () => {
         }
         setPokemons(filteredPokemons);
     };
+    useEffect(() => {
+        getPokemons()
+    });
     return (
         <>
             <Navbar pokemonFilter={pokemonFilter} onChangeGenerations={onChangeGenerations} />
@@ -93,7 +87,7 @@ export const Home = () => {
                     {pokemons.map((pokemon, key) => (
                         <Grid item xl={2} lg={3} md={4} sm={6} xs={12} key={key}>
                             <PokemonCard container md={{ height: 50 }} name={pokemon.data.name}
-                                image={generation <= 5 ? pokemon['data']['sprites']['versions']['generation-v']['black-white']['animated']['front_default'] : pokemon['data']['sprites']['front_default'] } types={pokemon.data.types} />
+                                image={generation <= 5 ? pokemon['data']['sprites']['versions']['generation-v']['black-white']['animated']['front_default'] : pokemon['data']['sprites']['front_default']} types={pokemon.data.types} />
                         </Grid>
                     ))}
                 </Grid>
